f70309e730a103c469b018ffceb0ae65b38ea2d8
Oskar <oskar.maslek@gmail.com>
2020-01-07 14:08:19 +0100

    Repair class to className

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,7 @@ import React from "react";
 class Result extends React.Component {
   render() {
     return (
-      <div class="result__container">
+      <div className="result__container">
         <div className="result-value">{this.props.result}</div>
         <div className="calculation-value">{this.props.calculation}</div>
       </div>
